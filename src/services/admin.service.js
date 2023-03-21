259d412bd3b711bdab4630c792f52207c313b508
ahmed haggag <ahmedhaggagrady@gmail.com>
2023-03-21 17:48:46 +0200

    fix admin login

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,4 +1,4 @@
 const { Staff } = require("../models");
 exports.count =  async () => await Staff.count({ where: { isAdmin: true }});
 exports.create = async (adminData) => await Staff.create({ ...adminData, isAdmin: true });
-exports.findOne = async (query) => await Staff.findOne({ ...query, isAdmin: true });;
+exports.findOne = async (query) => await Staff.findOne({ where: {...query, isAdmin: true }});;
